baa0338aa9524060eb0b3dafbf3d86f4df78d8c3
Eric Cartagena <cartagenaea@gmail.com>
2022-03-27 18:55:37 -0700

    Update authenticate.js

--- src/js/authenticate.js
+++ src/js/authenticate.js
@@ -34,109 +34,109 @@ class Authenticate extends React.Component {
   render() {
     let contactInfo = (
       <div>
-        <div class="row">
-          <div class="container-fluid">
-            <div class="jumbotron">
+        <div className="row">
+          <div className="container-fluid">
+            <div className="jumbotron">
               <h1>Welcome, Jane Doe!</h1>
             </div>
           </div>
         </div>
 
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="row jumbotron">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="row jumbotron">
                 <img src="./img/portrait_of_woman.png" />
                 <br /><br />
                 <strong><p>Professional Model and aspiring philanthropist</p></strong>
                 <p>Proin sit amet tempor augue, nec vulputate diam. Donec molestie arcu et metus laoreet sagittis. Nulla sed nisl elit. Vivamus sem velit, vestibulum sit amet quam vel, ultricies facilisis risus. Pellentesque id consequat nunc. Mauris ac posuere turpis. Proin at gravida lorem. Sed consequat justo ut tincidunt mattis.</p>
               </div>
               <br />
-              <div class="row">
-                <div class="card">
+              <div className="row">
+                <div className="card">
                   <h1>Friends</h1>
                 </div>
               </div>
             </div>
-            <div class="col-md-8">
-              <div class="jumbotron">
+            <div className="col-md-8">
+              <div className="jumbotron">
                 <h3>About</h3>
                 <br />
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-3">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-3">
                       <h6>First Name:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>Jane</p>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <h6>Last Name:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>Doe</p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-3">
+                  <div className="row">
+                    <div className="col-md-3">
                       <h6>Username:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>Janeee06</p>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <h6>Email:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>janeee06@email.com</p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-3">
+                  <div className="row">
+                    <div className="col-md-3">
                       <h6>City:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>Sacramento</p>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <h6>Country:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>United States</p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-3">
+                  <div className="row">
+                    <div className="col-md-3">
                       <h6>Birthday:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>January 01, 1996</p>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <h6>Interests:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>Web Development</p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-3">
+                  <div className="row">
+                    <div className="col-md-3">
                       <h6>Website:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>https://example.com/</p>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <h6>Phone:</h6>
                     </div>
-                    <div class="col-md-3">
+                    <div className="col-md-3">
                       <p>916-555-5555</p>
                     </div>
                   </div>
                 </div>
                 <hr />
-                <div class="row">
-                  <div class="col-md-6">
+                <div className="row">
+                  <div className="col-md-6">
                     <h3>Experience</h3>
                     <ul list-style-type="disc">
                       <li>CEO at Abc Fashion</li>
@@ -144,7 +144,7 @@ class Authenticate extends React.Component {
                       <li>Professional Model for Abercrombie</li>
                     </ul>
                   </div>
-                  <div class="col-md-6">
+                  <div className="col-md-6">
                     <h3>Education</h3>
                     <ul>
                       <li>American River College</li>
@@ -154,8 +154,8 @@ class Authenticate extends React.Component {
                   </div>
                 </div>
                 <hr />
-                <div class="container">
-                  <div class="row">
+                <div className="container">
+                  <div className="row">
                     <h3>Description</h3>
                     <p>Cras aliquet, orci quis facilisis pulvinar, augue mi maximus mauris, et consequat quam ex a dolor. Nullam ultrices, dolor nec scelerisque ullamcorper, nulla nulla congue lacus, sit amet eleifend odio ligula vitae ligula. Mauris et nisi leo. Fusce malesuada eros mauris, non dignissim nibh tempor vitae. Phasellus finibus quam justo, non maximus erat congue at. Aenean porttitor ullamcorper vestibulum. Mauris elit odio, molestie vel tellus id, placerat elementum velit. Nunc vulputate quam odio, at consequat nisl semper et. Aliquam sapien diam, pretium id purus ut, efficitur pharetra lectus.</p>
 
@@ -172,25 +172,25 @@ class Authenticate extends React.Component {
     );
 
     let login = (
-      <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-          <div class="container">
-            <div class="jumbotron form">
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6">
+          <div className="container">
+            <div className="jumbotron form">
               <form action="#" onSubmit={this.authorize}>
                 <label>Username</label>
                 <br />
-                <input class="form-control" type="text" name="user" placeholder="Username" />
+                <input className="form-control" type="text" name="user" placeholder="Username" />
                 <br />
                 <label>Password</label>
                 <br />
-                <input class="form-control" type="password" name="pass" placeholder="Password" />
+                <input className="form-control" type="password" name="pass" placeholder="Password" />
                 <br />
-                <button type="submit" class="btn btn-primary" onClick={this.authorize}>Enter</button>
+                <button type="submit" className="btn btn-primary" onClick={this.authorize}>Enter</button>
               </form>
             </div>
           </div>
-          <div class="col-md-3"></div>
+          <div className="col-md-3"></div>
         </div>
       </div>
     );
